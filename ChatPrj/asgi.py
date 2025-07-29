b6d6fa6f059958cb85e6c9ce64c6a8a192c37905
Rathnarajugudala <gudalarathnaraju821@gmail.com>
2025-07-29 21:41:18 +0530

    dfghjk

--- ChatPrj/asgi.py
+++ ChatPrj/asgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
 
-# imports
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from ChatApp import routing
